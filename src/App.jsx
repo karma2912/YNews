d8a2198235a899b36c88134ca3f0ce63c606c2e6
karma2912 <rajakyash23@gmail.com>
2024-09-06 22:26:26 +0530

    cddf

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ const App = () => {
     return (
       <>
       <Routes>
-        <Route path="/" element={<><Navbar/><News key="general" category="general" country="in" pageSize={10}/></>}/>
-        <Route path="/business" element={<><Navbar/><News key="business" category="business" country="in" pageSize={10}/></>}/>
-        <Route path="/health" element={<><Navbar/><News key="health" category="health" country="in" pageSize={10}/></>}/>
-        <Route path="/sports" element={<><Navbar/><News key="sports" category="sports" country="in" pageSize={10}/></>}/>
-        <Route path="/science" element={<><Navbar/><News key="science" category="science" country="in" pageSize={10}/></>}/>
-        <Route path="/technology" element={<><Navbar/><News key="technology" category="technology" country="in" pageSize={10}/></>}/>
-        <Route path="/entertainment" element={<><Navbar/><News key="entertainment" category="entertainment" country="in" pageSize={10}/></>}/>
+        <Route path="/" element={<><Navbar/><News key="general" category="general" country="us" pageSize={10}/></>}/>
+        <Route path="/business" element={<><Navbar/><News key="business" category="business" country="us" pageSize={10}/></>}/>
+        <Route path="/health" element={<><Navbar/><News key="health" category="health" country="us" pageSize={10}/></>}/>
+        <Route path="/sports" element={<><Navbar/><News key="sports" category="sports" country="us" pageSize={10}/></>}/>
+        <Route path="/science" element={<><Navbar/><News key="science" category="science" country="us" pageSize={10}/></>}/>
+        <Route path="/technology" element={<><Navbar/><News key="technology" category="technology" country="us" pageSize={10}/></>}/>
+        <Route path="/entertainment" element={<><Navbar/><News key="entertainment" category="entertainment" country="us" pageSize={10}/></>}/>
       </Routes>
       </>
     )
